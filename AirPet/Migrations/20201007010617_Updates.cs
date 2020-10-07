8038cecd7a511b1986b3188e361e9152a0fc45b5
Beverly Potts <pottsbeverly@gmail.com>
2020-10-06 21:24:15 -0400

    Troubleshoot no hosts listed on location details view

---
+++ AirPet/Migrations/20201007010617_Updates.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirPet.Migrations
+{
+    public partial class Updates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Hosts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pets",
+                table: "Hosts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Hosts");
+
+            migrationBuilder.DropColumn(
+                name: "Pets",
+                table: "Hosts");
+        }
+    }
+}
