aad4fec4ee1f54dd2dd031cfd8cadc24f0aba27d
Beverly Potts <pottsbeverly@gmail.com>
2020-10-06 21:03:51 -0400

    Build out basic views for hosts and locations

--- AirPet/Models/Host.cs
+++ AirPet/Models/Host.cs
@@ -4,6 +4,8 @@ namespace AirPet.Models
   {
     public int HostId { get; set; }
     public string Name { get; set; }
+    public string Pets { get; set; }
+    public string Bio { get; set; }
     public int LocationId { get; set; }
     public virtual Location Location { get; set; }
   }
